c2cf98f0cf7eaf1951d40845e6f6be8c9f9a6a96
Prathyusha <prathyusha_paleti@epam.com>
2025-06-03 16:07:08 +0530

    #Fixed unit test cases - React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import FilterBar from './components/filterbar/filterbar';
 import HeroImage from './components/heroimage/heroimage';
 import MoviesList from './components/movietile/movielist';
